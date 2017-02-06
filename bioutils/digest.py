85ffc23546fa8163c1fb17ffbcd25702dc9792b9
Reece Hart <reecehart@gmail.com>
2017-02-05 21:02:36 -0800

    fixed Digest on Python 2

--- bioutils/digest.py
+++ bioutils/digest.py
@@ -4,7 +4,7 @@ import six
 
 
 @six.python_2_unicode_compatible
-class Digest(six.binary_type):
+class Digest(bytes):
     """Represents a binary digest, with support for encoding and decoding using printable characters
 
     Stringified representations use URL-safe base64 encodings. See
@@ -12,16 +12,26 @@ class Digest(six.binary_type):
 
     >>> import hashlib
 
-    >>> d = Digest(hashlib.sha512().digest())
+    >>> b = hashlib.sha512().digest()
+    >>> len(b)
+    64
+
+    >>> d = Digest(b)
     >>> len(d)
     64
+    >>> len(d.as_base64us())
+    88
+    >>> d.as_base64us()
+    'z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg_SpIdNs6c5H0NE8XYXysP-DGNKHfuwvY7kxvUdBeoGlODJ6-SfaPg=='
     >>> str(d)
     'z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg_SpIdNs6c5H0NE8XYXysP-DGNKHfuwvY7kxvUdBeoGlODJ6-SfaPg=='
 
     >>> d24 = d[:24]
     >>> len(d24)
     24
-    >>> str(d24)
+    >>> len(d24.as_base64us())
+    32
+    >>> d24.as_base64us()
     'z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXc'
 
     >>> e = Digest.from_base64us(d.as_base64us())
@@ -34,13 +44,16 @@ class Digest(six.binary_type):
 
     """
 
-
     def __str__(self):
         """returns digest as URL-safe, base64 encoded digest, as Unicode"""
         return self.as_base64us().decode("ASCII")
 
-    def __getitem__(self, key):
-        return Digest(bytes(self)[key])
+    if six.PY2:
+        def __getslice__(self, start, end):
+            return Digest(bytes.__getslice__(self, start, end))
+    else:
+        def __getitem__(self, key):
+            return Digest(bytes.__getitem__(self, key))
 
     def as_base64us(self):
         """returns digest as URL-safe, base64-encoded digest, as ASCII-encoded binary"""
@@ -50,3 +63,14 @@ class Digest(six.binary_type):
     def from_base64us(d):
         """returns Digest object initialized from ASCII-encoded binary form of URL-safe, base64-encoded digest"""
         return Digest(base64.urlsafe_b64decode(d))
+
+
+if __name__ == "__main__":
+    import hashlib
+    b = hashlib.sha512().digest()
+    d = Digest(b)
+    assert isinstance(d, Digest), "d isn't a Digest"
+    d24 = d[:24]
+    assert isinstance(d24, Digest), "d24 isn't a Digest"
+    e = Digest.from_base64us(d.as_base64us())
+    e24 = Digest.from_base64us(d24.as_base64us())
