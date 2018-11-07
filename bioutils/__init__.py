18eebacbe7e670e56b27f46b5086e1433ffd4675
Reece Hart <reecehart@gmail.com>
2018-11-07 10:00:24 -0800

    fixed escape warning in regexp

--- bioutils/__init__.py
+++ bioutils/__init__.py
@@ -7,7 +7,7 @@ import sys
 
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
-    if re.match('^\d+\.\d+\.\d+$', __version__) is not None:
+    if re.match(r"^\d+\.\d+\.\d+$", __version__) is not None:
         _is_released_version = True  # pragma: no cover
 except pkg_resources.DistributionNotFound as e:  # pragma: no cover
     warnings.warn("can't get __version__ because %s package isn't installed" %
@@ -16,4 +16,4 @@ except pkg_resources.DistributionNotFound as e:  # pragma: no cover
 
 
 if sys.version_info < (3,5):
-    warnings.warn("Support for Python <3.6 will be dropped on 2019-03-31. See https://github.com/biocommons/org/wiki/Migrating-to-Python-3.6")
+    warnings.warn("Support for Python <3.6 will be dropped on 2019-03-31. See https://github.com/biocommons/org/wiki/Migrating-to-Python-3.6", category=DeprecationWarning)
