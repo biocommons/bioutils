26da47bd510287b7b339405c1fdac0ad0f0c1d4e
Reece Hart <reecehart@gmail.com>
2019-05-13 08:34:23 -0700

    emit version warning as log warning

--- bioutils/_versionwarning.py
+++ bioutils/_versionwarning.py
@@ -4,11 +4,15 @@ This module may be used by other biocommons packages
 
 """
 
+import logging
 import sys
 import warnings
 
+version_warning = ("biocommons tools are tested and supported only on Python >= 3.6"
+                   " (https://github.com/biocommons/org/wiki/Migrating-to-Python-3.6)")
+
+_logger = logging.getLogger(__package__)
+
 if sys.version_info < (3, 6):
-    warnings.warn(
-        "Support for Python < 3.6 is now deprecated and"
-        " will be dropped on 2019-03-31. See"
-        " https://github.com/biocommons/org/wiki/Migrating-to-Python-3.6")
+    _logger.warning(version_warning)
+    
