0d2c48451e94b8d908bd762aa20b0fb6f933a06d
Ryan Gomoto <7393416+gomoto@users.noreply.github.com>
2022-06-09 18:25:57 -0700

    test_trim_matching_alleles_error

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -19,6 +19,15 @@ normalize_vcf = partial(normalize_seq, mode=NormalizationMode.VCF)
 # normalize_expand_no_trim = partial(normalize_seq, mode=None, trim=True)
 
 
+@pytest.mark.parametrize('normalize_trim', [normalize_trim, normalize_trim_no_shuffle])
+def test_trim_matching_alleles_error(normalize_trim):
+    """Should raise error when trimming allele that matches reference allele."""
+    with pytest.raises(ValueError) as exc_info:
+        # (22, 25) on sequence is AGC
+        normalize_trim(interval=(22, 25), alleles=(None, "AGC"))
+    assert str(exc_info.value) == "Must have at least two distinct alleles to trim"
+
+
 def test_no_trim_no_shuffle():
     assert ((22, 25), ("AGC", "AGC")) == normalize_no_trim_no_shuffle(
         interval=(22, 25), alleles=(None, "AGC")
