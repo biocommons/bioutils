083acd4320e49673972b948d24140923ddfb0c1d
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-05-29 14:24:13 -0400

    chore: remove __future__ imports

--- src/bioutils/accessions.py
+++ src/bioutils/accessions.py
@@ -25,9 +25,6 @@ Some sample serializations of Identifiers:
 The string form may be used as a CURIE, in which case the document in
 which the CURIE is used must contain a map of ``{namespace : uri}``.
 """
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import re
 
 from .exceptions import BioutilsError
